4a72775b6a3382592c12a3dacff8998f40f7206d
Rizwan Nafees <rizwannafees13@gmail.com>
2024-10-31 21:47:40 -0400

    Initialized Node.js and backend folder structure

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+// test route 
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`)
+})
