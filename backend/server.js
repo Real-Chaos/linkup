8d1c5771f2d5e74f3b66bf84dd834182bf13e232
Rizwan Nafees <rizwannafees13@gmail.com>
2024-10-31 23:16:35 -0400

    Configured PostgreSQL connection and added dotenv configuration for environment variables

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const app = express()
+const pool = require('./db')
+
+pool.query('SELECT NOW()', (err, res) => {
+  if(err) console.error('Database connection error: ', err.stack)
+  else console.log('Database connected at: ', res.rows[0].now)
+})
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
