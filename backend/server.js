d0c85c8da79fb39d14638d597adf595e3b5d72b5
Rizwan Nafees <rizwannafees13@gmail.com>
2024-11-01 20:36:01 -0400

    Added basic routes for user registration and login

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const app = express()
 const pool = require('./db')
+const authRoutes = require('./routes/authRoutes')
+const userRoutes = require('./routes/userRoutes')
+
+
+// parse json
+app.use(express.json())
+
+// connect to database
 
 pool.query('SELECT NOW()', (err, res) => {
   if(err) console.error('Database connection error: ', err.stack)
   else console.log('Database connected at: ', res.rows[0].now)
 })
+
+// routes 
+app.use('/auth', authRoutes)
+app.use('/api', userRoutes)
+
+// initialize port
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
